d485764bd7b61b582c3a8513330c06e8f206e073
Ezeozue Zimuzo <zimuzostanley@gmail.com>
2013-07-13 22:55:16 +0000

    Second correction on web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync(index.html, "utf-8"));
+  response.send(fs.readFileSync("index.html", "utf-8"));
 });
 
 var port = process.env.PORT || 5000;
