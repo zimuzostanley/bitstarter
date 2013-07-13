3d2673dbc6e47ff54db62729655758de535ba105
Ezeozue Zimuzo <zimuzostanley@gmail.com>
2013-07-13 22:16:20 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
-var express = require('express');
+1;3201;0cvar express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
