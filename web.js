7a77a6c0fad13695072cba0f29aaedeff8c8711e
zimuzostanley <zimuzostanley@gmail.com>
2013-07-22 23:47:56 +0000

    Added static file support in web.js and revamped index.html, hope the mobile design finally works

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 var express = require('express');
 var fs = require('fs');
+var path = require('path');
 
 var app = express.createServer(express.logger());
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(request, response) {
   response.send(fs.readFileSync("index.html", "utf-8"));
 });
