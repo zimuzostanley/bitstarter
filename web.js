e71874bd2e053abfbbb7154066ae52b980323cd4
zimuzostanley <zimuzostanley@gmail.com>
2013-07-23 20:01:50 +0000

    MOdification to web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var path = require('path');
 
 var app = express.createServer(express.logger());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 app.get('/', function(request, response) {
   response.send(fs.readFileSync("index.html", "utf-8"));
 });
