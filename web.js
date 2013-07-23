bd5914c45b80f9bbafe64f5304aab3b57d771f30
zimuzostanley <zimuzostanley@gmail.com>
2013-07-23 20:11:21 +0000

    MOdification to web.js

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
+process.env.PWD = process.cwd();
 var express = require('express');
 var fs = require('fs');
 var path = require('path');
 
 var app = express.createServer(express.logger());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(process.env.PWD + '/public'));
 app.get('/', function(request, response) {
   response.send(fs.readFileSync("index.html", "utf-8"));
 });
