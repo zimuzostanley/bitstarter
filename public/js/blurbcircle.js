0fc41aeacd07e80754dc3e7ae7436e8fce3b8032
zimuzostanley <zimuzostanley@gmail.com>
2013-07-24 19:14:08 +0000

    Stable versions kinda of index.html css and js has add blurb option and slight modifications

--- public/js/blurbcircle.js
+++ public/js/blurbcircle.js
@@ -1,6 +1,20 @@
 $(function(){
-    alert('zim');
-    $('.loginBtn').click(function(){
+	$('.loginBtn').click(function(){
 	FB.login();
     });
+
+    $('.categoryBtn').click(function(){
+    	$('.board-location').hide();
+		$('.board-category').toggle();
+		$('.locationBtn').parent().removeClass('active');
+		$(this).parent().addClass('active');
+	});	
+
+	$('.locationBtn').click(function(){
+		$('.board-category').hide();
+		$('.board-location').toggle();
+		$('.categoryBtn').parent().removeClass('active');
+		$(this).parent().addClass('active');
+	});	
+
 });
