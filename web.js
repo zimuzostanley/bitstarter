e51fbbc0e882f9fd91d31529b649c4cbefd65c53
zimuzostanley <zimuzostanley@gmail.com>
2013-07-26 09:04:35 +0000

    routing testing

--- web.js
+++ web.js
@@ -6,6 +6,7 @@ var routes = require('./routes');
 
 var app = express.createServer(express.logger());
 
+app.set('port', process.env.PORT || 8080);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(app.router);
