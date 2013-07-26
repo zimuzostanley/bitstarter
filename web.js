28db56132dfdf8206b165b5d6732c9208a8b04a8
zimuzostanley <zimuzostanley@gmail.com>
2013-07-26 10:02:10 +0000

    Routing update

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var routes = require('./routes');
 var app = express.createServer(express.logger());
 
-app.set('port', process.env.PORT || 8080);
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(app.router);
