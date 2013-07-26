6c892e29b620b1764bb0be50dcd837098c59c0ba
zimuzostanley <zimuzostanley@gmail.com>
2013-07-26 09:25:44 +0000

    Routing testing

--- web.js
+++ web.js
@@ -1,9 +1,6 @@
 #!/usr/bin/env node
-
 var express = require('express');
-
 var routes = require('./routes');
-
 var app = express.createServer(express.logger());
 
 app.set('port', process.env.PORT || 8080);
@@ -12,7 +9,7 @@ app.set('view engine', 'ejs');
 app.use(app.router);
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', routes.index);
+app.get('/', routes.index
 
 
 
