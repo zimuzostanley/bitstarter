00b9fed1b0c232109b4eebc43c81cf46c89961a2
zimuzostanley <zimuzostanley@gmail.com>
2013-07-27 08:19:59 +0000

    Login button

--- web.js
+++ web.js
@@ -10,7 +10,7 @@ app.use(app.router);
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', routes.index);
-app.get('/login/:username/:loggedIn', function(req, res){
+app.get('/login', function(req, res){
     res.send('loginget');
 });
 app.get('/logout/:username/:loggedIn', routes.logout);
