99c9b57a97f42f4590f0768a2280548da094e4a5
zimuzostanley <zimuzostanley@gmail.com>
2013-08-17 14:46:51 +0000

    sequelize

--- models/blurber.js
+++ models/blurber.js
@@ -1,6 +1,6 @@
-module.exports = function(sequelize, DatTypes) {
+module.exports = function(sequelize, DataTypes) {
     return sequelize.define("Blurber", {
-
+	blurber_id: {type: DataTypes.BIGINT, unique: true, allowNull: false},
 	email: {type: DataTypes.STRING, unique: true, allowNull: true},
 	display_name: {type: DataTypes.STRING, allowNull: false},
 	date: {type: DataTypes.DATE, defaultValue: true},
