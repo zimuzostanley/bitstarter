8688a254862034cf478dc3d3dc5d98e51d4a7f19
zimuzostanley <zimuzostanley@gmail.com>
2013-08-17 14:07:22 +0000

    sequelize

--- models/blurb.js
+++ models/blurb.js
@@ -1,9 +1,9 @@
 module.exports = function(sequelize, DataTypes){
     return sequelize.define("Blurb", {
-	name: {type: DataTypes.STRING, allowNull: False},
-	start_price: {type: DataTypes.FLOAT, allowNull: False},
-	last_price: {type: DataTypes.FLOAT, allowNull: False},
-	description: {type: DataTypes.TEXT, allowNull: True},
+	name: {type: DataTypes.STRING, allowNull: false},
+	start_price: {type: DataTypes.FLOAT, allowNull: false},
+	last_price: {type: DataTypes.FLOAT, allowNull: false},
+	description: {type: DataTypes.TEXT, allowNull: true},
 	date: {type: DataTypes.DATE, defaultValue: DataTypes.NOW},
     });
 };
